923d8cc80e0557b3db2ff51017c75dcd961e468b
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-02 13:18:44 -0400

    added two end point routes for get reqs at api/notes for learning

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,35 @@
+const { error } = require('console');
 const fs = require('fs');
-const router = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
+const router = require('express').Router(); // declaring the Router Method from express to eport these routes for these html pages from GET reqs
+const { v4: uuidv4 } = require('uuid'); // 'Universally Unique Identifier' module 
 
+
+
+// define GET reqs to this route's end point @ './api/notes'
+router.get('./api/notes', async (req, res) =>{
+    const dB = await JSON.parse(fs.readFileSync('db/db.json', 'utf8')); 
+}); 
+// using 'await' INSIDE an async function BECAUSE readFileSync is synchronous
+// better way to handle a promise than .then
+// sets up route handler for HTTP GET reqs on path to ./api/notes
+// anonymous async function to handle reading file
+// readFileSync, synchronously reads 'db.json' and then returns it as a string
+// JSON.parse then parses the string and converts it to a js object
+// async await reads contents of db.json snychronously, reading as text and returning as string
+
+
+// alternate method
+// router.get('./api/notes', async (req, res)=>{
+//     try{ 
+//         const data = await fs.promises.readFile('db/db.json', 'utf8');
+//         const dB = JSON.parse(data)
+//         res.json(dB);
+//     } catch (error) {
+//     console.error(error)
+//     res.status(500).send('Server Error')
+//     }
+// });
+
+
+
+module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
 const path = require('path');
 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+}); // sets route for 'index.html' as response to client when GET req made to 'index.html'
+
+router.get('/notes', (req, res)=> {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+}) // sets route for 'notes.html' as response to clinet when GET req made to 'notes.html'
