8756d2b7bcb8a7f8d6cc0bb891cf3b804e27c82a
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-03 16:20:55 -0400

    readded pseudo code images w gitignore

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,12 @@
-const router = require('express').Router();
-const path = require('path');
+const router = require("express").Router();
+const path = require("path");
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 }); // route handler for get requests, sends HTML as responses
 
-router.get('/notes', (req, res)=> {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
-}) // route handler for get requests at /notes, sends HTML as response
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+}); // route handler for get requests at /notes, sends HTML as response
 
 module.exports = router;
