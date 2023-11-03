61f070f645c4893737c316460fc0ed6c327c6841
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-03 15:51:38 -0400

    troubleshooting deploy issues

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index'))
 }); // route handler for get requests, sends HTML as responses
 
 router.get('/notes', (req, res)=> {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
+    res.sendFile(path.join(__dirname, '../public/notes'))
 }) // route handler for get requests at /notes, sends HTML as response
 
 module.exports = router;
